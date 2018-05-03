acb38027ec1dde9fe2008751783d2cbed06eee8e
Daniel-I-Am <34800550+Daniel-I-Am@users.noreply.github.com>
2018-05-03 02:08:03 +0200

    Update component.lua

--- usr/config/baseControl/component.lua
+++ usr/config/baseControl/component.lua
@@ -1,1 +1,17 @@
-
+#please write the following format:
+#redstoneID;componentName;redstoneOutputSide;invertedstate
+#
+#A number sign can be used to comment
+#Invalid lines will be commented out upon runtime
+#
+#redstoneID is a 32 hex char long UUID
+#componentName can be anything, not including a ';'
+#redstoneOutputSide can be north,south,east,west,top or bottom
+#invertedstate can be true or false
+#Example:
+#aabbccdd-eeff-gghh-iijj-kkllmmnnoopp;Some {} Name;west;false
+#
+#If adding a reactor, please uncomment the following two lines (replacing the UUID):
+#<flux_gate_in_UUID>;flux_gate_in;none;false
+#<flux_gate_out_UUID>;flux_gate_out;none;false
+#
